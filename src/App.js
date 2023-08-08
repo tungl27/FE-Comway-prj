6c9dfc19f8df7b6b384fa71d69eb50bc74bdace4
LeQuocViet11012001 <lequocviet11012001@gmail.com>
2023-08-08 15:23:37 +0700

    login % staff list

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import "./App.css";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
-import Pagination from "./Components/Pagination/Pagination";
-import { Fragment, useState } from "react";
-import Breadcrumb from "./Components/BreadCrumb/BreadCrumb";
-import StaffList from "./View/StaffList/StaffList";
-import Login from "./View/Login/Login";
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header/Header';
+import Formmenu from './Components/FormMenu/FormMenu';
+import Footer from './Components/Footer/Footer';
+import Pagination from './Components/Pagination/Pagination';
+import { Fragment, useState } from 'react';
+import Breadcrumb from './Components/BreadCrumb/BreadCrumb';
 
-const breadcrumbs = [
-  { title: "メニュー", url: "menu" },
-  { title: "スタッフ一覧", url: "stafflist" },
-];
+const breadcrumbs = [{ title: 'メニュー', url: 'menu' }, { title: 'スタッフ一覧', url: 'stafflist' }]
 
 function App() {
-  const [active, setActive] = useState(2);
+
+  const [active, setActive] = useState(2)
 
   return (
     <Fragment>
-      <div className="App">
-        <Header></Header>
-        {/* <Login/> */}
-        <Breadcrumb breadcrumbs={breadcrumbs}></Breadcrumb>
-
-        <StaffList></StaffList>
-        {/* <Formmenu></Formmenu> */}
-
-        <Footer></Footer>
-      </div>
+      <Header></Header>
+      <Breadcrumb breadcrumbs={breadcrumbs}></Breadcrumb>
+      <Formmenu></Formmenu>
+      <Pagination activepage={active} totalRecords={50} pageSize={5} setActive={setActive}></Pagination>
+      <Footer></Footer>
     </Fragment>
   );
 }
